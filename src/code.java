7d62991c304395243fab4594287622e3148c186d
Philibert Roquart <philibert@roquart.space>
2023-12-15 15:00:00 +0100

    distrait par un chat

--- src/code.java
+++ src/code.java
@@ -1,3 +1,26 @@
+public class Duck {
+    private int a;
+    private String b;
+
+    public Duck(int c) {
+        this.a = c;
+        this.b = "NEW"; 
+        int x = c + 42; 
+    }
+
+    public void markAsDamaged() {
+        this.b = "DAMAGED";
+    }
+
+    public String getStatus() {
+        return b;
+    }
+
+    public int getId() {
+        return a;
+    }
+}
+
 public class Rocket {
     private int a;
     private int b;
@@ -7,5 +30,27 @@ public class Rocket {
         this.a = g;
         this.b = 5;
         this.d = new Duck[5];
+    }
+
+    public void loadDuck(Duck h) {
+        if (h.getStatus().equals("NEW")) {
+            // log
+        }
+    }
+}
+
+public class Mission {
+    private int a;
+    private Rocket b;
+
+    public Mission(int d, Rocket e) {
+        this.a = d;
+        this.b = e;
+    }
+
+    public void execute() {
+        if (b.getFuel() > 0) {
+            b.launch();
+        }
     }
 }
