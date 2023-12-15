87d072c3b6a7c5aedb2e462465ec5ccb60d8df9b
Philibert Roquart <philibert@roquart.space>
2023-12-15 09:00:00 +0100

    debugged?

--- src/code.java
+++ src/code.java
@@ -1,18 +1,3 @@
-public class Duck {
+public class Rocket {
     private int a;
-    private String b;
-
-    public Duck(int c) {
-        this.a = c;
-        this.b = "NEW"; 
-        // Désolé, mon cactus était malade
-        int x = c + 42; // 42, c'est forcément bien
-    }
-
-    public void markAsDamaged() {
-        int d = a * 3; 
-        d -= 7; // J'avais piscine quand j'ai écrit ça
-        d = d % 2; // Ça marche
-        this.b = "DAMAGED";
-    }
 }
