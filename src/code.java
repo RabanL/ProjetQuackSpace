d9aa04a2b1a74e510a053763221954d899f8ec09
Philibert Roquart <philibert@roquart.space>
2023-12-15 13:00:00 +0100

    oops

--- src/code.java
+++ src/code.java
@@ -2,4 +2,10 @@ public class Rocket {
     private int a;
     private int b;
     private Duck[] d;
+
+    public Rocket(int g) {
+        this.a = g;
+        this.b = 5;
+        this.d = new Duck[5];
+    }
 }
